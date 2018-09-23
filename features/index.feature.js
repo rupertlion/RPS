a9c78dbf75578bb89c472057a99e4862ad574d9d
rupertlion <rupertlion@gmail.com>
2018-09-23 13:07:36 +0200

    Updated index, trying to connect rps instance to DOM

--- features/index.feature.js
+++ features/index.feature.js
@@ -19,22 +19,22 @@ describe('User can input a value and get Rock Paper Scissors results', () => {
         await  browser.close();
     })
 
-    it('clicking on the "Play" button', async () => {
+    it('clicking on the "Check" button', async () => {
         await browser.fillIn("input[id='value']", { with:  "rock" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.eql('Computer wins!');
     })
 
-    it('clicking on the "Play" button', async () => {
+    it('clicking on the "Check" button', async () => {
         await browser.fillIn("input[id='value']", { with:  "paper" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.eql("It's a tie!");
     })
 
-    it('clicking on the "Play" button', async () => {
-        await browser.fillIn("input[id='value']", { with:  "scissors" })
+    it('clicking on the "Check" button', async () => {
+        await browser.fillIn("input[id='value']", { with:  "rock" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.eql('You win!');
