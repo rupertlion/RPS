ab00966a1a6a30d00a054b95090146b07a682b7f
rupertlion <rupertlion@gmail.com>
2018-09-23 12:16:49 +0200

    Added feature tests for result of playing

--- features/index.feature.js
+++ features/index.feature.js
@@ -1,6 +1,9 @@
 const { Rps } = require('../spec/spec.helper')
 const BrowserHelpers = require('e2e_training_wheels')
 const browser = new BrowserHelpers()
+const mockMath = Object.create(global.Math);
+mockMath.random = () => 0.5;
+global.Math = mockMath;
 
 describe('User can input a value and get Rock Paper Scissors results', () => {
     before(async () => {
@@ -14,5 +17,26 @@ describe('User can input a value and get Rock Paper Scissors results', () => {
 
     after(async ()=> {
         await  browser.close();
+    })
+
+    it('clicking on the "Play" button', async () => {
+        await browser.fillIn("input[id='value']", { with:  "rock" })
+        await browser.clickOnButton("input[value='Check']")
+        let content = await browser.getContent("[id='display_answer']")
+        expect(content).to.eql('You lose!');
+    })
+
+    it('clicking on the "Play" button', async () => {
+        await browser.fillIn("input[id='value']", { with:  "paper" })
+        await browser.clickOnButton("input[value='Check']")
+        let content = await browser.getContent("[id='display_answer']")
+        expect(content).to.eql("It's a tie!");
+    })
+
+    it('clicking on the "Play" button', async () => {
+        await browser.fillIn("input[id='value']", { with:  "scissors" })
+        await browser.clickOnButton("input[value='Check']")
+        let content = await browser.getContent("[id='display_answer']")
+        expect(content).to.eql('You win!');
     })
 })
