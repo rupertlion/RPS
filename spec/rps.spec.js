c9a9aaad03dfddd73f667ecd4071ba702feb5599
rupertlion <rupertlion@gmail.com>
2018-09-23 10:52:17 +0200

    Set up additional tests for winner and tie - failing

--- spec/rps.spec.js
+++ spec/rps.spec.js
@@ -1,10 +1,22 @@
 const { Rps } =  require('./spec.helper')
 
 describe('Rps', () => {
-    let rps = new Rps
+    let rps = new Rps;
 
     it('returns an error if no choice is made', () => {
         expect(rps.check(null)).to.eql('Hey come on, be serious, give me a choice!')
+    })
+
+    it('calculates the winner of the game', () => {
+        let choice1 = 'rock';
+        let choice2 = 'scissors';
+        expect(rps(choice1, choice2)).to.equal('rock wins')
+    })
+
+    it('calculates when a game is a tie', () => {
+        let choice1 = 'rock';
+        let choice2 = 'rock';
+        expect(rps(choice1, choice2)).to.equal('The result is a tie! Try again.')
     })
 
 })
--- src/js/rps.js
+++ src/js/rps.js
@@ -1,7 +1,36 @@
-function Rps()  {
-    this.check = (input) => {
-        if (!input) {
+function Rps(choice1,choice2) {
+        
+    this.check = (choice1) => {
+        if (!choice1) {
             return 'Hey come on, be serious, give me a choice!';
+        }
+    }
+
+    if (choice1 === choice2) {
+        return "The result is a tie! Try again.";
+        }
+    
+    if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "rock wins";
+        } else { 
+            return "paper wins";
+        }
+    }
+    
+    if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "paper wins";
+        } else { 
+            return "scissors wins";
+        }
+    }
+    
+    if (choice1 === "scissors") {
+        if (choice2 === "paper") {
+            return "scissors wins";
+        } else { 
+            return "rock wins";
         }
     }
 }
