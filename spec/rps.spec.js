aba7767e7b802accc083cc12524fc90afd4ed3ec
rupertlion <rupertlion@gmail.com>
2018-09-23 12:01:33 +0200

    Added describe blocks to organise tests for winner and tie situations

--- spec/rps.spec.js
+++ spec/rps.spec.js
@@ -4,49 +4,58 @@ describe('Rps', () => {
     
     let rps = new Rps
 
-    it('returns an error if no choice is made', () => {
-        expect(rps.check(null)).to.eql('Hey come on, be serious, give me a choice!')
+    describe('Input required', () => {
+        it('returns an error if no choice is made', () => {
+            expect(rps.check(null)).to.eql('Hey come on, be serious, give me a choice!')
+        })
     })
 
-    it('expects Player 1 to win the game', () => {
-        expect(rps.check('rock', 'scissors')).to.equal('rock wins')
+    describe('Player 1 wins', () => {
+        it('expects Player 1 to win the game', () => {
+            expect(rps.check('rock', 'scissors')).to.equal('rock wins')
+        })
+
+        it('expects Player 1 to win the game', () => {
+            expect(rps.check('scissors', 'paper')).to.equal('scissors wins')
+        })
+
+        it('expects Player 1 to win the game', () => {
+            expect(rps.check('paper', 'rock')).to.equal('paper wins')
+        })
+
     })
 
-    it('expects Player 1 to win the game', () => {
-        expect(rps.check('scissors', 'paper')).to.equal('scissors wins')
+    describe('Player 2 wins', () => {
+        it('expects Player 2 to win the game', () => {
+            expect(rps.check('rock', 'paper')).to.equal('paper wins')
+        })
+
+        it('expects Player 2 to win the game', () => {
+            expect(rps.check('scissors', 'rock')).to.equal('rock wins')
+        })
+
+        it('expects Player 2 to win the game', () => {
+            expect(rps.check('paper', 'scissors')).to.equal('scissors wins')
+        })
+
     })
 
-    it('expects Player 1 to win the game', () => {
-        expect(rps.check('paper', 'rock')).to.equal('paper wins')
-    })
+    describe('Game is tied', () => {
+        it('calculates when a game is a rock tie', () => {
+            expect(rps.check('rock', 'rock')).to.equal('The result is a tie! Try again.')
+        })
 
-    it('expects Player 2 to win the game', () => {
-        expect(rps.check('rock', 'paper')).to.equal('paper wins')
-    })
+        it('calculates when a game is a paper tie', () => {
+            expect(rps.check('paper', 'paper')).to.equal('The result is a tie! Try again.')
+        })
 
-    it('expects Player 2 to win the game', () => {
-        expect(rps.check('scissors', 'rock')).to.equal('rock wins')
-    })
+        it('calculates when a game is a scissors tie', () => {
+            expect(rps.check('scissors', 'scissors')).to.equal('The result is a tie! Try again.')
+        })
 
-    it('expects Player 2 to win the game', () => {
-        expect(rps.check('paper', 'scissors')).to.equal('scissors wins')
+        it('creates a random choice for the computer', () => {
+            computerchoice = 0.1;
+            expect(choice2).to.equal('rock')
+        })
     })
-
-    it('calculates when a game is a rock tie', () => {
-        expect(rps.check('rock', 'rock')).to.equal('The result is a tie! Try again.')
-    })
-
-    it('calculates when a game is a paper tie', () => {
-        expect(rps.check('paper', 'paper')).to.equal('The result is a tie! Try again.')
-    })
-
-    it('calculates when a game is a scissors tie', () => {
-        expect(rps.check('scissors', 'scissors')).to.equal('The result is a tie! Try again.')
-    })
-
-    it('creates a random choice for the computer', () => {
-        computerchoice = 0.1;
-        expect(choice2).to.equal('rock')
-    })
-
 })
