a6db3d41cffbba1cb073f6f3257717ab9a9d2f82
rupertlion <rupertlion@gmail.com>
2018-10-06 12:28:10 +0200

    Cleaned up testing comments

--- features/index.feature.js
+++ features/index.feature.js
@@ -22,18 +22,4 @@ describe('User can input a value and get Rock Paper Scissors results', () => {
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.include('chose');
     })
-
-    // it('clicking on the "Check" button', async () => {
-    //     await browser.selectOption("select[id='value']", { option:  "paper" })
-    //     await browser.clickOnButton("input[value='Check']")
-    //     let content = await browser.getContent("[id='display_answer']")
-    //     expect(content).to.eql("It's a tie! You both chose rock");
-    // })
-
-    // it('clicking on the "Check" button', async () => {
-    //     await browser.selectOption("select[id='value']", { option:  "scissors" })
-    //     await browser.clickOnButton("input[value='Check']")
-    //     let content = await browser.getContent("[id='display_answer']")
-    //     expect(content).to.eql('You win! The computer chose scissors');
-    // })
 })
