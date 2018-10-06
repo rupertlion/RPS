1c1eb78519b41b80532169f2408f2561b98c4a09
rupertlion <rupertlion@gmail.com>
2018-10-06 12:05:39 +0200

    Updated feature test to account for dropdown vs input field

--- features/index.feature.js
+++ features/index.feature.js
@@ -17,21 +17,21 @@ describe('User can input a value and get Rock Paper Scissors results', () => {
     })
 
     it('clicking on the "Check" button', async () => {
-        await browser.fillIn("input[id='value']", { with:  "rock" })
+        await browser.selectOption("select[id='value']", { option:  "rock" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.eql('Computer wins! The computer chose paper');
     })
 
     it('clicking on the "Check" button', async () => {
-        await browser.fillIn("input[id='value']", { with:  "paper" })
+        await browser.selectOption("select[id='value']", { option:  "paper" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.eql("It's a tie! You both chose paper");
     })
 
     it('clicking on the "Check" button', async () => {
-        await browser.fillIn("input[id='value']", { with:  "scissors" })
+        await browser.selectOption("select[id='value']", { option:  "scissors" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.eql('You win! The computer chose paper');
