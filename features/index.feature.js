90e7b4e106fb3eaca9ffb469904e9c76f7096b62
rupertlion <rupertlion@gmail.com>
2018-10-06 12:24:45 +0200

    Update feature tests for correct message

--- features/index.feature.js
+++ features/index.feature.js
@@ -27,13 +27,13 @@ describe('User can input a value and get Rock Paper Scissors results', () => {
         await browser.selectOption("select[id='value']", { option:  "paper" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
-        expect(content).to.eql("It's a tie! You both chose paper");
+        expect(content).to.eql("It's a tie! You both chose rock");
     })
 
     it('clicking on the "Check" button', async () => {
         await browser.selectOption("select[id='value']", { option:  "scissors" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
-        expect(content).to.eql('You win! The computer chose paper');
+        expect(content).to.eql('You win! The computer chose scissors');
     })
 })
