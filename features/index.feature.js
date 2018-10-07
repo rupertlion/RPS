e6500860cb14821064fdf2aa1e49521a41749ad0
rupertlion <rupertlion@gmail.com>
2018-10-07 12:21:46 +0200

    Refactored test descriptions for easier reading in tests

--- features/index.feature.js
+++ features/index.feature.js
@@ -23,21 +23,21 @@ describe('User can input a value and get Rock Paper Scissors results', () => {
         await  browser.close();
     })
 
-    it('clicking on the "Check" button', async () => {
+    it('choosing rock and clicking "Check" button', async () => {
         await browser.selectOption("select[id='value']", { option:  "rock" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.include('Computer wins! The computer chose paper');
     })
 
-    it('clicking on the "Check" button', async () => {
+    it('choosing paper and clicking "Check" button', async () => {
         await browser.selectOption("select[id='value']", { option:  "paper" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
         expect(content).to.include("It's a tie! You both chose paper");
     })
 
-    it('clicking on the "Check" button', async () => {
+    it('choosing scissors clicking "Check" button', async () => {
         await browser.selectOption("select[id='value']", { option:  "scissors" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
