06659d6eb8896cb5dda4f3e22751d943519fd90c
rupertlion <rupertlion@gmail.com>
2018-10-07 11:50:40 +0200

    Attempting to require sinon and ransomise function through spec helper and in inxed.feature

--- features/index.feature.js
+++ features/index.feature.js
@@ -1,4 +1,5 @@
 const { Rps } = require('../spec/spec.helper')
+const { Randomise } = require('../spec/spec.helper')
 const BrowserHelpers = require('e2e_training_wheels')
 const browser = new BrowserHelpers()
 
@@ -20,7 +21,7 @@ describe('User can input a value and get Rock Paper Scissors results', () => {
     it('clicking on the "Check" button', async () => {
         let sinon = require('sinon')
         let fake = sinon.fake.returns('paper');
-        sinon.replace(randomise, fake);
+        sinon.replace(this, randomise, fake);
         await browser.selectOption("select[id='value']", { option:  "rock" })
         await browser.clickOnButton("input[value='Check']")
         let content = await browser.getContent("[id='display_answer']")
